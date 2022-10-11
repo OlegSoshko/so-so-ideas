a1ebf6195f0d1e2995997f232fbeea87ceb9b5d0
Oleg Soshko <oleg.91.03@gmail.com>
2022-10-11 23:40:31 +0300

    Updates

---
+++ 67.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkideas=self.webpackChunkideas||[]).push([[67],{67:(e,s,n)=>{n.r(s),n.d(s,{Test:()=>u});var t=n(294),u=function(){return t.createElement("div",null,"ПРивет это лейзи")}}}]);
